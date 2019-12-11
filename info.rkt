d82feab1ecdebd3153cdc121e925fef490d1900c
srfoster <stephen@thoughtstem.com>
2019-12-11 11:37:59 -0800

    travis

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "website-js")
-(define deps '("base"))
+(define deps '("base" "gregor"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/js.scrbl" ())))
 (define pkg-desc "Description Here")

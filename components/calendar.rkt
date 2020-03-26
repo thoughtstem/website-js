a1551729d53f0b83468a85611f6eed86e58a0963
srfoster <stephen@thoughtstem.com>
2020-03-25 18:20:31 -0700

    adding calendar2

--- components/calendar.rkt
+++ components/calendar.rkt
@@ -1,6 +1,8 @@
 #lang at-exp racket
 
-(provide calendar event)
+(provide calendar 
+         calendar2
+         event)
 
 (require website-js
          "./time-select.rkt"
@@ -26,6 +28,101 @@
   (card 
     class: "bg-success text-white"
     (card-header n)))
+
+(define (calendar2 the-date (events (hash)))
+  (define month-name
+    (~t the-date "MMMM"))
+
+  ;returns day of week month starts on
+  (define start-wday
+    (->wday the-date))
+
+  (enclose
+   (define (normal-day n)
+     (day-square #:id (~a (ns 'dayTarget) n) n events))
+
+    (define day-names
+      (div
+        class: "card-group"
+        (day-name "Sun") 
+        (day-name "Mon") 
+        (day-name "Tue") 
+        (day-name "Wed")
+        (day-name "Thu") 
+        (day-name "Fri") 
+        (day-name "Sat")))
+
+    (define week-1-end-wday
+      (- 7 (sub1 start-wday)))
+
+    (define week-1 
+      (card-group
+       (map (thunk* (day-square "" #:active? #f))
+            (range start-wday))
+       (map normal-day (range 1 week-1-end-wday))))
+
+    (define week-2-end-wday (+ 7 week-1-end-wday))
+
+    (define week-2
+      (card-group
+       (map normal-day (range week-1-end-wday week-2-end-wday))))
+
+    (define week-3-end-wday (+ 7 week-2-end-wday))
+
+    (define week-3
+      (card-group
+       (map normal-day (range week-2-end-wday week-3-end-wday))))
+
+    (define week-4-end-wday (+ 7 week-3-end-wday))
+
+    (define week-4
+      (card-group
+       (map normal-day (range week-3-end-wday week-4-end-wday))))
+
+    (define week-5-end-day
+      (days-in-month (->year the-date)
+                     (->month the-date)))
+
+    (define week-5-end-wday
+      (->wday (date (->year the-date)
+                    (->month the-date)
+                    week-5-end-day)))
+
+    (define week-5
+      (card-group
+       (map normal-day (range week-4-end-wday
+                              (add1 week-5-end-day)))
+
+       (map (thunk* (day-square "" #:active? #f))
+            (range (- 7 (add1 week-5-end-wday))))))
+
+    (card id: (ns 'main) 
+     (card-header 
+      (h3 month-name))
+     (card-body 
+      ;(button-danger on-click: (call 'advanceDay) (i class: "fas fa-clock") " Advance")
+      day-names
+      week-1 
+      week-2
+      week-3
+      week-4
+      week-5))
+    (script ([dayTarget (ns 'dayTarget)]
+             [currentDay 0]
+             [contruct @js{setTimeout(()=>{@(call 'advanceDay)},1)}])
+            (function (advanceDay)
+               (set-var currentDay @js{@currentDay + 1})
+               @js{
+                 document.querySelectorAll("@(id# 'main) .dayTarget").forEach(function(e){ 
+                   @(-> 'e setNotCurrent)
+                 })
+               }
+               (-> @getEl{@dayTarget + @currentDay}
+                         setCurrent))
+            (function (addEvent day event)
+                      (-> @getEl{@dayTarget + day}
+                          addEvent
+                          event)))))
 
 (define (calendar the-date (events (hash)))
   (define month-name
